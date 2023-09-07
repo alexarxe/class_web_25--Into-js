d36874264e3f91b3de6c9e0d01c1a3d2daa8870f
Alex Arce <92236695+alexarxe@users.noreply.github.com>
2023-09-06 20:32:47 -0400

    Initial Commit

---
+++ ejercicio01/js/script.js
@@ -0,0 +1,23 @@
+document.getElementById('title').innerHTML='Mi primer JavaScript'
+
+var saludar;
+nombre = "Alex";
+saludo = "Hola ";
+
+saludar = (saludo + nombre);
+
+alert(saludar);
+
+/* Operaciones arigmeticas */
+alert(5+5);
+
+//concatenar texto con el numero
+alert("10"+5);
+
+
+var a, b, resultado;
+a = 5;
+b = 10;
+resultado = a + b;
+
+alert("La suma de " + a + " + " + b + " es= " + resultado)
---
+++ ejercicio02/js/script.js
@@ -0,0 +1,19 @@
+function msj() 
+{
+    alert("Este es un ejemplo de JavaScript");
+}
+
+function salida() 
+{
+    var paso=11;
+        if(paso==11)
+            alert("Paso es igual a 11");
+}
+
+function bucle() 
+{
+    var cont;
+        for(cont = 1; cont <= 5; cont ++) {
+            alert("Contador" + cont)
+        }
+}
